769e090e58c5e2340b41c7b2b799dffacd6ca1aa
developer9000 <obluchkov@gmail.com>
2019-11-18 20:11:33 -0400

    added solution for addTime

--- week1/addTime/index.js
+++ week1/addTime/index.js
@@ -5,5 +5,24 @@
  * @returns {String}
  */
 module.exports = function (hours, minutes, interval) {
-
+    function validTime(hours,minutes) {
+        if (hours<10) {
+        hours = '0' + hours;
+        }
+        if (minutes<10) {
+        minutes = '0' + minutes;
+        }
+        if (hours>=24) {
+        hours = '00';
+        }
+        return hours + ':' + minutes;
+    }
+    var temp=(minutes+interval)/60;
+    if (temp>=1) {
+        hours+=Math.floor(temp);
+        minutes=(minutes+interval)-Math.floor(temp)*60;
+        return validTime(hours,minutes);
+    } else {
+        return validTime(hours,minutes+interval);
+    }
 };
